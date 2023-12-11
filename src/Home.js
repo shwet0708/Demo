fd1b910f3e3315d396d9d4b50ce535ffb66f16ef
shwet0708 <shwetbhungaliya4321@gmail.com>
2023-12-11 15:19:03 +0530

    Demo

---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Home = () => {
+    return(
+        <>
+        <h1>Hello ishita sonani divya</h1>
+        </>
+    )
+}
+export default Home;
---
+++ src/table.js
@@ -0,0 +1,11 @@
+
+const StudentData = () => {
+ 
+  return (
+    <>
+      
+    </>
+  );
+};
+
+export default StudentData;
